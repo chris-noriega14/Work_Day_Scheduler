57b7fb25fa6381aa8e91e4c58e792e04d9471d42
chris-noriega14 <chris.noriega14@hotmail.com>
2021-09-30 19:24:25 -0500

    Added all time slots and changed event description font color to black

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,4 @@
+//This displays the today's date and time on the header.
+//This time will be used to help identify which boxes to turn gray, red, or green.
 var CurrentTime = moment().format('LLLL');
 $("#currentDay").text(CurrentTime);
