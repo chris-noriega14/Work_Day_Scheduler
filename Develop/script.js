a3bf2634ab93ee11fed11c8ee82e9c3fe1463b7c
chris-noriega14 <chris.noriega14@hotmail.com>
2021-10-08 14:51:56 -0500

    Added refresh and color coding based on past, present, or future time slot functionality

--- Develop/script.js
+++ Develop/script.js
@@ -7,31 +7,18 @@ $("#currentDay").text(CurrentTime);
 // var keepData = localStorage.setItem(textarea);
 
 var hourBlock = moment().startOf('day').add(9,'h').format("H"); //hour of first block (used for testing)
-var currentHour = moment().format("H") //Current hour (24 hour format)
+var currentHour = moment().hours(); //Current hour (24 hour format)
 
 //This section attampts to retain the text from the description section once the page has refreshed.
 function PageReload () {
     if(location.reload(true)) {
-        document.getElementById("hour9").value = localStorage.getItem("timeStamp","valueStamp");
+        // document.getElementById("hour9").value = localStorage.getItem("timeStamp","valueStamp");
         console.log("Test");
     }
 }
 
 //This section of code compares the current time of day vs. the time on the time slots.
 //This helps us figure out which color the description boxes need to be.
-for (var i = 8; i < 17; i++) {
-    var DescBGColor;
-
-    if(currentHour == i){
-        DescBGColor = 'present';
-    } 
-    else if (currentHour > i) {
-        DescBGColor = 'past';
-    }
-    else if (currentHour < i) {
-        DescBGColor = 'future';
-    }      
-};
 
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
@@ -43,9 +30,25 @@ $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
 
+function updateCSS () {
+    $(".description").each(function() {
+        var divBlock = parseInt($(this).parent().attr("id"));
+        if(divBlock < currentHour){
+            $(this).addClass("past");
+        }
+        else if (divBlock == currentHour) {
+            $(this).addClass("present");
+        }
+        else if (divBlock > currentHour) {
+            $(this).addClass("future");
+        }      
+    })
+}
+updateCSS();
 //This code adds the time slot and description to local storage once you click the save button.
 $(".saveBtn").on("click", function() {
     var valueStamp = $(this).siblings(".description").val();
     var timeStamp = $(this).parent().attr("id");
     localStorage.setItem(timeStamp,valueStamp);
 });
+
